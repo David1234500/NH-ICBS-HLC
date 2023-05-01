8a8743f2f998fcb9e289e75ec9ccd6b4d37db2cd
David Kluener <david.kluener@rwth-aachen.de>
2023-05-01 09:13:56 +0000

    added new mp set

--- jobs.sh
+++ jobs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+set -e
 export LD_LIBRARY_PATH=/usr/local/lib/
 
-cd build
-./mp_generate_nlopt_mstart_staged
-
-echo "eval_enterable"
-./eval_enterable
-
-echo "eval_leaveable"
-./eval_leaveable
+./eval_config_two_vehicles_leavable_dual_rev 25 25 50 50 2 2
+./eval_config_two_vehicles_leavable_dual_rev 25 25 50 50 3 3
+./eval_config_two_vehicles_leavable_dual_rev 25 25 50 50 1 1
+./eval_config_two_vehicles_leavable_dual_rev 70 70 50 50 1 1
+./eval_config_two_vehicles_leavable_dual_rev 10 10 40 40 1 1
+./eval_config_two_vehicles_leavable_dual_rev 5 5 40 40 1 1
+./eval_config_two_vehicles_leavable_dual_rev 5 5 40 40 5 5
+./eval_config_two_vehicles_leavable_dual_rev 5 5 40 40 9 9
 
 echo "JOBS DONE"
